388479ae76a90577e3910f4466ee4d1ba803bee0
Ben Pitzer <ben.pitzer@gmail.com>
2022-07-29 00:00:28 -0700

    Enable video patterns

--- visualization/server.py
+++ visualization/server.py
@@ -67,15 +67,15 @@ class PatternGenerator:
     def __init__(self, led_config):
         self.result = asyncio.Future()
         config = [
-            # (VideoPattern, dict(file='media/butter_churn.mp4',
-            #  fps=20, crop=Rect(60, 60, 60, 60))),
-            # (VideoPattern, dict(file='media/psychill1.mp4',
-            #  fps=20, crop=Rect(60, 130, 60, 60))),
-            # (VideoPattern, dict(file='media/psychill2.mp4',
-            #  fps=20, crop=Rect(60, 130, 60, 60))),
+            (VideoPattern, dict(file='media/butter_churn.mp4',
+             fps=20, crop=Rect(60, 60, 60, 60))),
+            (VideoPattern, dict(file='media/psychill1.mp4',
+             fps=20, crop=Rect(60, 130, 60, 60))),
+            (VideoPattern, dict(file='media/psychill2.mp4',
+             fps=20, crop=Rect(60, 130, 60, 60))),
             (RGTRansitionPattern, dict()),
-            # (VideoPattern, dict(file='media/milkdrop.mp4',
-            #  fps=20, crop=Rect(60, 130, 60, 60)))
+            (VideoPattern, dict(file='media/milkdrop.mp4',
+             fps=20, crop=Rect(60, 130, 60, 60)))
         ]
 
         self.patterns = []
