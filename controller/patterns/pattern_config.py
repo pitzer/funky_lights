3f0782acea86d7801634d36e804c1d8f99969dab
Ben Pitzer <ben.pitzer@gmail.com>
2022-08-03 22:39:56 -0700

    Some tweaks

--- controller/patterns/pattern_config.py
+++ controller/patterns/pattern_config.py
@@ -1,12 +1,12 @@
 from patterns.fire_pattern import FirePattern, FirePatternUV, PALETTE_FIRE
 from patterns.rg_transition_pattern import RGTRansitionPattern
-from patterns.solid_color_blink import SolidColorBlinkPattern, COLOR_PALETTE_TROPICAL
+from patterns.solid_color_blink import SolidColorBlinkPattern, PALETTE_TROPICAL
 from patterns.video_pattern import VideoPattern, Rect
 
 
 DEFAULT_CONFIG = [
     (FirePatternUV, dict(palette=PALETTE_FIRE, width=2, height=100)),
-    (SolidColorBlinkPattern, dict(colors=COLOR_PALETTE_TROPICAL)),
+    (SolidColorBlinkPattern, dict(palette=PALETTE_TROPICAL)),
     (VideoPattern, dict(file='media/butter_churn.mp4', fps=20, crop=Rect(60, 60, 60, 60))),
     (VideoPattern, dict(file='media/psychill1.mp4', fps=20, crop=Rect(60, 130, 60, 60))),
     (VideoPattern, dict(file='media/psychill2.mp4', fps=20, crop=Rect(60, 130, 60, 60))),
--- controller/patterns/solid_color_blink.py
+++ controller/patterns/solid_color_blink.py
@@ -1,16 +1,15 @@
 from patterns.pattern import Pattern
 import numpy as np
-import time
 
-COLOR_PALETTE_TROPICAL = np.array([[242, 207, 51], [245, 112, 76], [32, 158, 179], [240, 167, 141]])
+PALETTE_TROPICAL = np.array([[242, 207, 51], [245, 112, 76], [32, 158, 179], [240, 167, 141]])
 
 class SolidColorBlinkPattern(Pattern):
     def __init__(self):
         super().__init__()
-        self.colors = None
+        self.palette = None
         self.current_color_index = 0
         self.fps = 0.5
-        self.cumulative_delta = 0
+        self.cumulative_delta = 1000
 
     def initialize(self):
         pass
@@ -19,8 +18,8 @@ class SolidColorBlinkPattern(Pattern):
         self.cumulative_delta += delta
         if self.cumulative_delta < 1 / self.fps:
             return
-        self.current_color_index = (self.current_color_index + 1) % len(self.colors)
+        self.current_color_index = (self.current_color_index + 1) % self.palette.shape[0]
         for segment in self.segments:
             for color in segment.colors:
-                np.copyto(color, self.colors[self.current_color_index])
+                np.copyto(color, self.palette[self.current_color_index])
         self.cumulative_delta = 0
