1953ac089d23aae966ba4dede9f8cd78d35ef0c0
Ben Pitzer <ben.pitzer@gmail.com>
2022-08-05 00:25:51 -0700

    Add launchpad handler to change the patterns

--- controller/main.py
+++ controller/main.py
@@ -2,6 +2,7 @@ import json
 import time
 import sys
 import asyncio
+import lpminimk3 
 import serial_asyncio
 import websockets
 import functools
@@ -26,6 +27,15 @@ def PrepareTextureMsg(segments):
     # Added padding to the end of the buffer to fill the full texture
     msg += [0] * (texture_size - len(msg))
     return bytearray(msg)
+
+
+def run_in_executor(f):
+    @functools.wraps(f)
+    def inner(*args, **kwargs):
+        loop = asyncio.get_running_loop()
+        return loop.run_in_executor(None, lambda: f(*args, **kwargs))
+
+    return inner
 
 
 async def ws_serve(websocket, generator):
@@ -63,18 +73,78 @@ class SerialWriter(asyncio.Protocol):
                     self.transport.serial.write(
                         messages.PrepareLedMsg(segment.uid, segment.colors))
 
+
+class LaunchPadHandler:
+    def __init__(self):
+        self.lp = None
+        self.buttons_active = []
+        self.buttons_pressed = []
+        self.LED_COLOR_ACTIVE = 100
+        self.LED_COLOR_INACTIVE = 0
+
+    def activate_button(self, button_name):
+        if not self.lp: 
+            return
+        button_group = self.lp.panel.buttons(button_name)
+        for button in button_group:
+            button.led.color = self.LED_COLOR_ACTIVE
+            if not button in self.buttons_active:
+                self.buttons_active.append(button)
+    
+    def deactivate_button(self, button_name):
+        if not self.lp:
+            return
+        button_group = self.lp.panel.buttons(button_name)
+        for button in button_group:
+            button.led.color = self.LED_COLOR_INACTIVE
+            if button in self.buttons_active:
+                self.buttons_active.remove(button)
+
+    def handle_event(self, button_event):
+        if button_event and button_event.type == lpminimk3.ButtonEvent.PRESS:
+            self.buttons_pressed.append(button_event.button.name)
+        elif button_event and button_event.type == lpminimk3.ButtonEvent.RELEASE:
+            pass
+
+    @run_in_executor
+    def poll(self):
+        self.handle_event(self.lp.panel.buttons().poll_for_event())
+
+    async def run(self):
+        available_lps = lpminimk3.find_launchpads()
+        if available_lps:
+            self.lp = available_lps[0]  # Use the first available launchpad
+            self.lp.open()  # Open device for reading and writing on MIDI interface (by default)
+            self.lp.mode = lpminimk3.Mode.PROG  # Switch to the programmer mode
+        else:
+            print(f"No launchpad found.")
+            return
+
+        for button in self.lp.panel.buttons():
+            button.led.color = self.LED_COLOR_INACTIVE
+        while True:
+            # Wait for a button press/release
+            await self.poll()
+
+
 class PatternGenerator:
-    def __init__(self, led_config):
+    def __init__(self, led_config, launchpad):
+        self.launchpad = launchpad
         self.result = asyncio.Future()
         self.patterns = []
         self.current_pattern_index = 0
-        for cls, params in pattern_config.DEFAULT_CONFIG:
+        self.button_to_pattern_index_map = {}
+        self.pattern_index_to_button_map = {}
+
+        for i, (button, cls, params) in enumerate(pattern_config.DEFAULT_CONFIG):
             pattern = cls()
             for key in params:
                 setattr(pattern, key, params[key])
             pattern.prepareSegments(led_config)
             pattern.initialize()
             self.patterns.append(pattern)
+            self.button_to_pattern_index_map[button] = i
+            self.pattern_index_to_button_map[i] = button
 
     async def tick(self, pattern, delta):
         pattern.animate(delta)
@@ -82,7 +152,7 @@ class PatternGenerator:
     async def run(self):
         ANIMATION_RATE = 20
         FPS_UPDATE_RATE = 1
-        PATTERN_DURATION = 120
+        MAX_PATTERN_DURATION = 5
 
         prev_animation_time = time.time() - 1.0 / ANIMATION_RATE
         prev_pattern_time = time.time()
@@ -91,11 +161,32 @@ class PatternGenerator:
         while True:
             cur_animation_time = time.time()
 
-            # Rotate patterns
-            if (cur_animation_time - prev_pattern_time) > PATTERN_DURATION:
+            # Check if a button was pressed on launchpad to change the pattern
+            if self.launchpad.buttons_pressed:
+                button = self.launchpad.buttons_pressed[-1]
+                if button in self.button_to_pattern_index_map:
+                    # Deactivate button corresponding to previous pattern
+                    self.launchpad.deactivate_button(
+                        self.pattern_index_to_button_map[self.current_pattern_index])
+                    # Update pattern index based on button press
+                    self.current_pattern_index = self.button_to_pattern_index_map[button]
+                    prev_pattern_time = cur_animation_time
+                self.launchpad.buttons_pressed.clear()
+            
+            # Check if max pattern time is exceeded
+            if (cur_animation_time - prev_pattern_time) > MAX_PATTERN_DURATION:
+                # Deactivate button corresponding to previous pattern
+                self.launchpad.deactivate_button(
+                    self.pattern_index_to_button_map[self.current_pattern_index])
+                # Rotate patterns
                 self.current_pattern_index = (
                     self.current_pattern_index + 1) % len(self.patterns)
                 prev_pattern_time = cur_animation_time
+
+            # Activate button corresponding to current pattern
+            self.launchpad.activate_button(
+                self.pattern_index_to_button_map[self.current_pattern_index])
+
             pattern = self.patterns[self.current_pattern_index]
 
             # Process animation
@@ -113,7 +204,7 @@ class PatternGenerator:
             # Output update rate to console
             counter += 1
             if (time.time() - start_time) > 1.0 / FPS_UPDATE_RATE:
-                print("Animation FPS: ", counter / (time.time() - start_time))
+                print("Animation FPS: %.1f" % (counter / (time.time() - start_time)))
                 counter = 0
                 start_time = time.time()
 
@@ -130,11 +221,15 @@ async def main():
         led_config = json.load(f)
     with open(bus_config_file, 'r') as f:
         bus_config = json.load(f)
-        
-    # Start pattern generator
-    pattern_generator = PatternGenerator(led_config)
-    asyncio.create_task(pattern_generator.run())
 
+    # Launchpad handler
+    launchpad_handler = LaunchPadHandler()
+    asyncio.create_task(launchpad_handler.run())
+
+    # Start pattern generator
+    pattern_generator = PatternGenerator(led_config, launchpad_handler)
+    asyncio.create_task(pattern_generator.run())
+    
     # Start WS server
     ws_serve_handler = functools.partial(ws_serve, generator=pattern_generator)
     await websockets.serve(ws_serve_handler, '127.0.0.1', WEB_SOCKET_PORT)
--- controller/patterns/pattern_config.py
+++ controller/patterns/pattern_config.py
@@ -4,12 +4,12 @@ from patterns.video_pattern import VideoPattern, Rect
 
 
 DEFAULT_CONFIG = [
-    (FirePatternUV, dict(palette=PALETTE_FIRE, width=2, height=100)),
-    (VideoPattern, dict(file='media/butter_churn.mp4', fps=20, crop=Rect(60, 60, 60, 60))),
-    (VideoPattern, dict(file='media/psychill1.mp4', fps=20, crop=Rect(60, 130, 60, 60))),
-    (VideoPattern, dict(file='media/psychill2.mp4', fps=20, crop=Rect(60, 130, 60, 60))),
-    (VideoPattern, dict(file='media/milkdrop.mp4', fps=20, crop=Rect(60, 130, 60, 60))),
-    (RGTRansitionPattern, dict()),
+    ('0x0', FirePatternUV, dict(palette=PALETTE_FIRE, width=2, height=100)),
+    ('1x0', VideoPattern, dict(file='media/butter_churn.mp4', fps=20, crop=Rect(60, 60, 60, 60))),
+    ('2x0', VideoPattern, dict(file='media/psychill1.mp4', fps=20, crop=Rect(60, 130, 60, 60))),
+    ('3x0', VideoPattern, dict(file='media/psychill2.mp4', fps=20, crop=Rect(60, 130, 60, 60))),
+    ('4x0', VideoPattern, dict(file='media/milkdrop.mp4', fps=20, crop=Rect(60, 130, 60, 60))),
+    ('5x0', RGTRansitionPattern, dict()),
 ]
 
 
