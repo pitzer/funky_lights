9144148b3974c7a7d33e23395bc5d8da804599e7
Ben Pitzer <ben.pitzer@gmail.com>
2022-08-06 23:34:44 -0700

    Switch meteor to sweep pattern

--- controller/patterns/pattern_config.py
+++ controller/patterns/pattern_config.py
@@ -1,13 +1,13 @@
 from patterns.fire_pattern import FirePattern, FirePatternUV
 from patterns.crossfade_pattern import CrossfadePattern
 import patterns.palettes as palettes
-from patterns.meteor_pattern import MeteorPattern
 from patterns.rg_transition_pattern import RGTRansitionPattern
+from patterns.sweep_pattern import SweepPattern
 from patterns.video_pattern import VideoPattern, Rect
 
 
 DEFAULT_CONFIG = [
-    (MeteorPattern, dict()),
+    (SweepPattern, dict()),
     (FirePatternUV, dict(palette=palettes.FIRE, width=2, height=100)),
     (CrossfadePattern, dict()),
     (VideoPattern, dict(file='media/butter_churn.mp4', fps=20, crop=Rect(60, 60, 60, 60))),
