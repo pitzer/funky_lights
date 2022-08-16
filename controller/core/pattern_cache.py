0f3cefcf415081b7a12cccb115066c0a12952cc0
Ben Pitzer <ben.pitzer@gmail.com>
2022-08-15 22:29:42 -0700

    Patterns stored as one file per animation step

--- controller/core/pattern_cache.py
+++ controller/core/pattern_cache.py
@@ -1,21 +1,45 @@
+from math import floor
 from patterns.pattern import Pattern
+from aiofile import async_open
 from copy import deepcopy
 import hashlib
 import json
-import shelve
+import math
+import os
+import pickle
 
+
+def hash_led_config(led_config):
+    led_config_bytes = bytearray(json.dumps(led_config, sort_keys=True).encode('ascii'))
+    return hashlib.sha256(led_config_bytes).hexdigest()
+
+
+def cache_file_path(led_config_hash, pattern_index, animation_index):
+    index_low = math.floor(animation_index / 1000) * 1000
+    index_high = math.floor(animation_index / 1000 + 1) * 1000 - 1
+    index_folder = '%06d-%06d' % (index_low, index_high)
+    filename = '%06d.p' % animation_index
+    return os.path.join(
+        '/tmp', 'pattern_cache', str(led_config_hash), str(pattern_index), index_folder, filename)
 
 class CachedPattern(Pattern):
-    def __init__(self):
+    def __init__(self, num_animation_steps, led_config_hash, pattern_index):
         super().__init__()
-        self.colors_timeseries = []
-        self.current_index = 0
+        self.num_animation_steps = num_animation_steps
+        self.led_config_hash = led_config_hash
+        self.pattern_index = pattern_index
+        self.current_animation_index = 0
 
-    def animate(self, delta):
-        segment_colors = self.colors_timeseries[self.current_index]
-        for i, segment in enumerate(self.segments):
-            segment.colors = segment_colors[i]
-        self.current_index = (self.current_index + 1) % len(self.colors_timeseries)
+    async def animate(self, delta):
+        cache_file = cache_file_path(
+            self.led_config_hash, self.pattern_index, self.current_animation_index)
+        async with async_open(cache_file, 'rb') as afp:
+            bytes = await afp.read()
+            segment_colors = pickle.loads(bytes)
+            for i, segment in enumerate(self.segments):
+                segment.colors = segment_colors[i]
+
+        self.current_animation_index = (self.current_animation_index + 1) % self.num_animation_steps
 
 
 class PatternCache:
@@ -23,46 +47,36 @@ class PatternCache:
         self.pattern_config = pattern_config
         self.led_config = led_config
         self.animation_rate = args.animation_rate
+        self.led_config_hash = hash_led_config(led_config)
+        print("LED config hash: " + str(self.led_config_hash))
+
+    async def build_cache(self, patterns, max_pattern_duration):
+        cached_patterns = []
+        delta = 1.0 / self.animation_rate
+        num_animation_steps = int(max_pattern_duration * self.animation_rate)
         
-        led_config_hash = hashlib.sha256(bytearray(json.dumps(
-            self.led_config, sort_keys=True).encode('ascii'))).hexdigest()
-        print("LED config hash: " + str(led_config_hash))
-        self.cache = shelve.open('/tmp/pattern_cache_' + str(led_config_hash))
+        for pattern_index, _ in enumerate(self.pattern_config):
+            pattern = patterns[pattern_index]
 
-    def build_cache(self, max_pattern_duration):
-        patterns = []
-        for pattern_index, config in enumerate(self.pattern_config):
-            pattern_index = str(pattern_index)
-            if not pattern_index in self.cache:
-                # Initialize pattern
-                (_, cls, params) = config
-                pattern = cls()
-                for key in params:
-                    setattr(pattern.params, key, params[key])
-                pattern.prepareSegments(self.led_config)
-                pattern.initialize()
-
-                # Animate pattern to created cached colors
-                print("Caching pattern %s of type %s" % (pattern_index, type(pattern).__name__))
-                cached_pattern = CachedPattern()
-                cached_pattern.prepareSegments(self.led_config)
-                cached_pattern.initialize()
-                duration = 0
-                delta = 1.0 / self.animation_rate
-                while duration <= max_pattern_duration:
-                    pattern.animate(delta)
-                    segment_colors = []
-                    for segment in pattern.segments:
-                        segment_colors.append(deepcopy(segment.colors))
-                    cached_pattern.colors_timeseries.append(segment_colors)
-                    duration += delta
-
-                # Add to cache and sync
-                self.cache[pattern_index] = cached_pattern
-                self.cache.sync()
-                patterns.append(cached_pattern)
-            else:
-                pattern = self.cache[pattern_index]
-                patterns.append(pattern)
+            # Animate pattern and cache color arrays for each step
+            print("Caching pattern %d of type %s" % (pattern_index, type(pattern).__name__))
+            cached_pattern = CachedPattern(num_animation_steps, self.led_config_hash, pattern_index)
+            cached_pattern.prepareSegments(self.led_config)
         
-        return patterns
+            for animation_index in range(num_animation_steps):
+                await pattern.animate(delta)
+                segment_colors = []
+                for segment in pattern.segments:
+                    segment_colors.append(deepcopy(segment.colors))
+
+                cache_file = cache_file_path(
+                    self.led_config_hash, pattern_index, animation_index)
+                os.makedirs(os.path.dirname(cache_file), exist_ok=True)
+                async with async_open(cache_file, 'wb') as afp:
+                    bytes = pickle.dumps(segment_colors)
+                    await afp.write(bytes)
+
+            # Add to cache and sync
+            cached_patterns.append(cached_pattern)
+
+        return cached_patterns
