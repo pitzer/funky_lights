f2dd72ad2480801d8b7099992bf351655242a550
Jake Fuentes <jake@cascade.io>
2022-08-17 23:02:32 -0700

    Cleanup

--- controller/patterns/pattern_config.py
+++ controller/patterns/pattern_config.py
@@ -17,11 +17,15 @@ DEFAULT_CONFIG = [
     # Standard patterns
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> c388583 (Cleanup)
     ('0x0', FirePatternUV, dict(palette=palettes.FIRE, width=2, height=100)),
     ('1x0', CrossfadePattern, dict()),
     ('2x0', TheaterChasePattern, dict(color=np.array([255, 255, 255], dtype=np.uint8), sparkle_probability=0.001, decay_param=0.95)),
     ('3x0', SweepPattern, dict(color=np.array([255, 255, 255], dtype=np.uint8), decay_param=0.5, sweep_speed=0.3)),
     ('4x0', ColorRollPattern, dict()),
+<<<<<<< HEAD
 =======
     # ('0x0', FirePatternUV, dict(palette=palettes.FIRE, width=2, height=100)),
     # ('1x0', CrossfadePattern, dict()),
@@ -37,17 +41,19 @@ DEFAULT_CONFIG = [
     # ('3x0', SweepPattern, dict(color=np.array([255, 255, 255], dtype=np.uint8), decay_param=0.5, sweep_speed=0.3)),
     # ('4x0', ColorRollPattern, dict()),
 >>>>>>> f434be0 (pattern updates)
+=======
+>>>>>>> c388583 (Cleanup)
 
     # Video patterns
-    # ('0x1', VideoPattern, dict(file='media/shifter_escape.mp4')),
-    # ('1x1', VideoPattern, dict(file='media/radial_beams.mp4', crop=Rect(0, 0, 850, 720))),
-    # ('2x1', VideoPattern, dict(file='media/butter_churn.mp4', crop=Rect(60, 60, 60, 60))),
-    # ('3x1', VideoPattern, dict(file='media/psychill1.mp4', fps=10)),
-    # ('4x1', VideoPattern, dict(file='media/psychill1.mp4', crop=Rect(60, 130, 60, 60))),
-    # ('5x1', VideoPattern, dict(file='media/psychill2.mp4', crop=Rect(60, 130, 60, 60))),
+    ('0x1', VideoPattern, dict(file='media/shifter_escape.mp4')),
+    ('1x1', VideoPattern, dict(file='media/radial_beams.mp4', crop=Rect(0, 0, 850, 720))),
+    ('2x1', VideoPattern, dict(file='media/butter_churn.mp4', crop=Rect(60, 60, 60, 60))),
+    ('3x1', VideoPattern, dict(file='media/psychill1.mp4', fps=10)),
+    ('4x1', VideoPattern, dict(file='media/psychill1.mp4', crop=Rect(60, 130, 60, 60))),
+    ('5x1', VideoPattern, dict(file='media/psychill2.mp4', crop=Rect(60, 130, 60, 60))),
     
     # # Effect patterns
-    # ('0x2', SparklePattern, dict(color=np.array([255, 255, 255]), sparkle_probability=0.001, decay_param=0.95)),
+    ('0x2', SparklePattern, dict(color=np.array([255, 255, 255]), sparkle_probability=0.001, decay_param=0.95)),
     ('1x2', RainbowWavesPattern, dict()),
-    # ('1x3', BouncingBlocksPattern, dict())
+    ('1x3', BouncingBlocksPattern, dict())
 ] 
