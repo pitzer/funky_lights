d2d39e8333070a339d9c0595e0e1b9aea28b2ab5
Ben Pitzer <ben.pitzer@gmail.com>
2022-08-21 14:36:34 -0700

    Shorten cache to 60s

--- controller/create_pattern_cache.py
+++ controller/create_pattern_cache.py
@@ -14,7 +14,7 @@ async def main():
     parser.add_argument("-a", "--animation_rate", type=int, default=20, help="The target animation rate in Hz")
     parser.add_argument("-f", "--force_update", action='store_true', 
                         help="Forces update of all cached patterns. Otherwise will only update missing or incomplete patterns.")
-    parser.add_argument("-m", "--max_cached_pattern_duration", type=int, default=600, 
+    parser.add_argument("-m", "--max_cached_pattern_duration", type=int, default=60, 
                         help="The maximum duration a pattern is cached for")
     args = parser.parse_args()
     led_config = json.load(args.led_config)
