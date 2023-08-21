f75064d50f300c3c4b7d503070002c7f99e91dc5
Ben Pitzer <ben.pitzer@gmail.com>
2023-08-21 15:12:44 -0700

    Update LED count script

--- controller/count_leds.py
+++ controller/count_leds.py
@@ -1,9 +1,8 @@
 import sys
-import time
 from funky_lights import connection, messages
 
-NUM_LEDS = 230
-WHITE = 255
+NUM_LEDS = 20
+
 
 def main():
     tty_device = connection.DEFAULT_TTY_DEVICE
@@ -18,27 +17,18 @@ def main():
         uid = int(sys.argv[3])
     if len(sys.argv) > 4:
         NUM_LEDS = int(sys.argv[4])
-  
+
     # Some test pattern
-    rgbs = []
-    for i in range(int(NUM_LEDS / 2)):
-        col = int(i / (NUM_LEDS / 2) * WHITE)
-        rgbs += [(col, WHITE-col, 0)]
-    for i in range(int(NUM_LEDS / 2)):
-        col = int(WHITE - (i / (NUM_LEDS / 2) * WHITE))
-        rgbs += [(col, WHITE - col, 0)]
     rgbs = [(255, 255, 255)] * NUM_LEDS
     rgbs[-1] = (255, 0, 0)
-
 
     serial_port = connection.InitializeController(
         tty_device, baudrate=baudrate)
 
     # Send messages to all the bars
-    for i in range(100000):
+    while True:
         serial_port.write(messages.PrepareLedMsg(uid, rgbs))
-        # rgbs = rgbs[1:] + rgbs[:1]
-        time.sleep(0.02)
+        NUM_LEDS = int(input("Number of LEDs: "))
 
     serial_port.close()
 
