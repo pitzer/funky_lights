85bdd06494d6de4d575a2057e7034e741d87e2ee
Ben Pitzer <ben.pitzer@gmail.com>
2024-01-15 00:37:59 -0800

    Use per head texture updates.

--- visualization/js/Sidebar.Objects.js
+++ visualization/js/Sidebar.Objects.js
@@ -10,6 +10,11 @@ import * as BufferGeometryUtils from '../js/BufferGeometryUtils.js';
 import headConfigData from '../../config/head_config.json' assert { type: 'json' };
 import ledConfigData from '../../config/led_config_impossible_dialogue.json' assert { type: 'json' };
 
+// constants
+const yOffset = 0.55;
+const textureWidth = 128;
+const textureHeight = 128;
+const cubeWidth = 0.03;
 
 function SidebarObjects( editor ) {
 
@@ -18,69 +23,52 @@ function SidebarObjects( editor ) {
     const signals = editor.signals;
 	const container = new UISpan();
 
-    // constants
-    const yOffset = 0.55;
+    const ledObjects = new Map();
+    const ledObjectMaterials = new Map();
 
-    // Create a 3D cube for each LED
-    const textureWidth = 128;
-    const textureHeight = 128;
-    const cubeWidth = 0.03;
-    var baseGeometry = new THREE.BoxGeometry(cubeWidth, cubeWidth, cubeWidth);
-    var cubes = [];
-    var index = 0;
-    for (var segment of ledConfigData.led_segments) {
-        for (var led_positions of segment.led_positions) {
-            var geometry = baseGeometry.clone();
-            geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(led_positions[0], led_positions[1], led_positions[2]));
-            // Colors are in order of the LED config. The texture is sized to a power of 2 for more efficient processing by the GPU.
-            const u = Math.floor(index / textureWidth) / (textureHeight - 1);
-            const v = (index % textureWidth) / (textureWidth - 1);
-            var uvAttribute = geometry.attributes.uv;
-            for (var i = 0; i < uvAttribute.count; i++) {
-                // Not sure why v and u are flipped here. I would have expected them the other way around
-                uvAttribute.setXY(i, v, u);
+    const loader = new THREE.FileLoader();
+
+    // Load object config
+    loader.load(
+        '../../config/head_config.json',
+
+        function (data) {
+            let json = JSON.parse(data);
+            // Create head objects
+            for (var head of json.heads) {
+                createObject(head);
             }
-            cubes.push(geometry);
-            index = index + 1;
+        },
+
+        // onProgress callback
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+
+        // onError callback
+        function (err) {
+            console.error(err);
         }
-    }
+    );
 
-    // Merge all geometries into one buffer for fast rendering.
-    const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries(cubes, false);
-    const size = textureWidth * textureHeight;
-    const initialTextureData = new Uint8Array(4 * size);
-    for (let i = 0; i < size; i++) {
-        const stride = i * 4;
-        initialTextureData[stride] = 0;
-        initialTextureData[stride + 1] = 0;
-        initialTextureData[stride + 2] = 0;
-        initialTextureData[stride + 3] = 255;
-    }
-    const texture = new THREE.DataTexture(initialTextureData, textureWidth, textureHeight);
-    texture.needsUpdate = true;
-    var material = new THREE.MeshBasicMaterial({ map: texture });
-
-    // Create head objects
-    for (var head of headConfigData.heads) {
-        const id = head.id;
-        const position = head.position;
-        const orientation = head.orientation;
-        const loader = new OBJLoader();
-        loader.load(
-            // resource URL
-            head.mesh,
-            // called when resource is loaded
+    // Create a 3D object based on the config.
+    function createObject(object_config) {
+        const object_id = object_config.id;
+        const position = object_config.position;
+        const orientation = object_config.orientation;
+        const led_config = object_config.led_config;
+        const obj_loader = new OBJLoader();
+        // Load mesh geometry
+        obj_loader.load(
+            object_config.mesh,
             function (object) {
-                object.name = id;
+                object.name = object_id;
                 object.position.set(position.x, position.y + yOffset, position.z);
                 object.rotateY(orientation * THREE.MathUtils.DEG2RAD)
                 editor.execute(new AddObjectCommand(editor, object));
 
                 // Add lights as a child
-                var lightsObject = new THREE.Mesh(mergedGeometry, material);
-                lightsObject.name = 'lights';
-                editor.execute(new AddObjectCommand(editor, lightsObject, object, 0));
-                editor.select(object);
+                createLightsObject(object_id, led_config, object);
             },
             // called when loading is in progresses
             function (xhr) {
@@ -88,10 +76,81 @@ function SidebarObjects( editor ) {
             },
             // called when loading has errors
             function (error) {
-                console.log('An error happened');
                 console.log(error);
             }
         );
+    }
+
+    // Create LED lights object with texture.
+    function createLightsObject(object_id, led_config_url, parent_object) {
+        const loader = new THREE.FileLoader();
+        loader.load(
+            led_config_url,
+
+            function (data) {
+                let json = JSON.parse(data);
+                const mergedGeometry = createBufferGeometriesForLeds(json);
+                const material = createMaterialForLeds();
+                var lightsObject = new THREE.Mesh(mergedGeometry, material);
+                lightsObject.name = 'lights';
+                ledObjects.set(object_id, lightsObject);
+                ledObjectMaterials.set(object_id, material);
+                editor.execute(new AddObjectCommand(editor, lightsObject, parent_object, 0));
+                editor.select(parent_object);
+            },
+
+            // onProgress callback
+            function (xhr) {
+                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+            },
+
+            // onError callback
+            function (err) {
+                console.error(err);
+            }
+        );
+    }
+
+    // Create a 3D cube for each LED and merge into a buffer.
+    function createBufferGeometriesForLeds(ledConfig) {
+        var baseGeometry = new THREE.BoxGeometry(cubeWidth, cubeWidth, cubeWidth);
+        var cubes = [];
+        var index = 0;
+        for (var segment of ledConfig.led_segments) {
+            for (var led_positions of segment.led_positions) {
+                var geometry = baseGeometry.clone();
+                geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(led_positions[0], led_positions[1], led_positions[2]));
+                // Colors are in order of the LED config. The texture is sized to a power of 2 for more efficient processing by the GPU.
+                const u = Math.floor(index / textureWidth) / (textureHeight - 1);
+                const v = (index % textureWidth) / (textureWidth - 1);
+                var uvAttribute = geometry.attributes.uv;
+                for (var i = 0; i < uvAttribute.count; i++) {
+                    // Not sure why v and u are flipped here. I would have expected them the other way around
+                    uvAttribute.setXY(i, v, u);
+                }
+                cubes.push(geometry);
+                index = index + 1;
+            }
+        }
+
+        // Merge all geometries into one buffer for fast rendering.
+        return BufferGeometryUtils.mergeBufferGeometries(cubes, false);
+    }
+    
+    // Create a texture material shared by all LEDs of an object.
+    function createMaterialForLeds() {
+        const size = textureWidth * textureHeight;
+        const initialTextureData = new Uint8Array(4 * size);
+        for (let i = 0; i < size; i++) {
+            const stride = i * 4;
+            initialTextureData[stride] = 0;
+            initialTextureData[stride + 1] = 0;
+            initialTextureData[stride + 2] = 0;
+            initialTextureData[stride + 3] = 255;
+        }
+        const texture = new THREE.DataTexture(initialTextureData, textureWidth, textureHeight);
+        texture.needsUpdate = true;
+        return new THREE.MeshBasicMaterial({ map: texture });
     }
 
     // Start listening to websockets for LED updates
@@ -103,6 +162,7 @@ function SidebarObjects( editor ) {
                 let texture = json[i];
                 const object_id = texture.object_id;
                 const data = Uint8Array.from(atob(texture.texture_data), c => c.charCodeAt(0))
+                let material = ledObjectMaterials.get(object_id);
                 material.map = new THREE.DataTexture(data, textureWidth, textureHeight);
                 material.map.needsUpdate = true;
             }
