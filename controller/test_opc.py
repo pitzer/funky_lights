49b0e6c7187d2c0b7730f517c721d063dc134c94
Ben Pitzer <ben.pitzer@gmail.com>
2024-03-19 23:36:07 -0700

    Test program for OPC communication

---
+++ controller/test_opc.py
@@ -0,0 +1,25 @@
+from opclib import opc
+import time
+
+ADDRESS = '192.168.86.52:7890'
+
+# Create a client object
+client = opc.Client('192.168.86.52:7890')
+
+# Test if it can connect (optional)
+if client.can_connect():
+    print('connected to %s' % ADDRESS)
+else:
+    # We could exit here, but instead let's just print a warning
+    # and then keep trying to send pixels in case the server
+    # appears later
+    print('WARNING: could not connect to %s' % ADDRESS)
+
+# Send pixels forever at 30 frames per second
+while True:
+    my_pixels = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+    if client.put_pixels(my_pixels, channel=0):
+        print('...')
+    else:
+        print('not connected')
+    time.sleep(1/30.0)
